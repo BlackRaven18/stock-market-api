f56c7c81906c21cd354372be7a79dcd7f7d3feea
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2025-02-04 18:13:25 +0100

    Updated Comments database schema

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
-        public string Conent { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
         public int? StockId { get; set; }
         public Stock? Stock { get; set; }
