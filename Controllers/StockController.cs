7702894502b89e3ae5cec54440dfe7a41dc9874a
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2025-02-04 12:26:28 +0100

    Added stock controller

---
+++ Controllers/StockController.cs
@@ -0,0 +1,37 @@
+﻿using dotnet_web_api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_web_api.Controllers
+{
+    [ApiController]
+    [Route("api/stock")]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if(stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+    }
+}
