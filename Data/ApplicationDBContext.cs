ea517c5b72c1dcd8aa572a175f842ee5916f8886
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2025-02-04 09:07:53 +0100

    Added database configuration

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using dotnet_web_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_web_api.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
