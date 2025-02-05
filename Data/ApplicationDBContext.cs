3391a8f0ed4bb879987f6996d8c8817359129608
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2025-02-05 15:09:07 +0100

    Added JWT configuration

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,9 +1,10 @@
 ﻿using dotnet_web_api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_web_api.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions)
             : base(dbContextOptions)
---
+++ Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace dotnet_web_api.Models
+{
+    public class AppUser :IdentityUser
+    {
+
+    }
+}
