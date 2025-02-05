39091b2878b00df381f8576fe4aa6fa82c84aff6
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2025-02-05 16:45:46 +0100

    Added User registration

---
+++ DTOs/Account/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_web_api.DTOs.Account
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+    }
+}
