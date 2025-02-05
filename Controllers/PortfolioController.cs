6af2900cd23e5b1745fd945159b0757424a6d9b1
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2025-02-05 21:06:46 +0100

    Added endpoint to get user stocks

---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,37 @@
+﻿using dotnet_web_api.Extensions;
+using dotnet_web_api.Interfaces;
+using dotnet_web_api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_web_api.Controllers
+{
+    [ApiController]
+    [Route("api/portfolio")]
+
+    public class PortfolioController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IStockRepository _stockRepo;
+        private readonly IPortfolioRepository _portfolioRepo;
+
+        public PortfolioController(UserManager<AppUser> userManager, IStockRepository stockRepo, IPortfolioRepository portfolioRepo)
+        {
+            _userManager = userManager;
+            _stockRepo = stockRepo;
+            _portfolioRepo = portfolioRepo;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUserPortfolio()
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser);
+
+            return Ok(userPortfolio);
+        }
+    }
+}
---
+++ Interfaces/IPortfolioRepository.cs
@@ -0,0 +1,9 @@
+﻿using dotnet_web_api.Models;
+
+namespace dotnet_web_api.Interfaces
+{
+    public interface IPortfolioRepository
+    {
+        Task<List<Stock>> GetUserPortfolio(AppUser user);
+    }
+}
