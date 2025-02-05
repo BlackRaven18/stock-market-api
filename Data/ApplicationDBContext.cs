a21b94160e1f75b96ed7736b845681ed6b3fb056
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2025-02-05 16:45:33 +0100

    Added SeedRoles migration

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,4 +1,6 @@
-﻿using dotnet_web_api.Models;
+﻿using dotnet_web_api.Migrations;
+using dotnet_web_api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,5 +16,27 @@ namespace dotnet_web_api.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "f8b36c48-2f43-4c0b-9311-5db9b1a5b9c1",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "a1234567-b890-1234-c567-890123456789",
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
